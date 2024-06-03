046599ca1b1edcf0a695ca256aa90a0e64cbae57
fazliddinrasulov <fazliddin0815@gmail.com>
2024-06-03 17:58:06 +0500

    Hateoas (Inline) with Rest API and Exceptions

--- src/main/java/uz/pdp/test/controller/StudentController.java
+++ src/main/java/uz/pdp/test/controller/StudentController.java
@@ -5,12 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
 import org.springframework.hateoas.Link;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import uz.pdp.test.entity.Student;
 import uz.pdp.test.exception.ItemNotFoundException;
@@ -28,22 +23,30 @@ public class StudentController {
     @GetMapping
     public CollectionModel<?> getAllStudent() {
         List<Student> students = studentService.getAllStudents();
-        Link self = linkTo(methodOn(StudentController.class).getAllStudent()).withRel("self");
-        return CollectionModel.of(students, self);
+        return CollectionModel.of(
+                students,
+                Link.of("http://localhost:8080/students").withRel("self")
+        );
     }
 
     @GetMapping("{id}")
     public EntityModel<?> getStudent(@PathVariable Integer id) {
-        Link self = linkTo(methodOn(StudentController.class).getStudent(id)).withRel("self");
-        Link all= linkTo(methodOn(StudentController.class).getAllStudent()).withRel("all");
         Student student = studentService.getStudent(id).orElseThrow(() -> new ItemNotFoundException("Bunday o'quvchi mavjud emas"));
-        return EntityModel.of(student,self, all);
+        return EntityModel.of(
+                student,
+                Link.of("http://localhost:8080/students/" + id).withRel("self"),
+                Link.of("http://localhost:8080/students").withRel("all")
+        );
     }
 
     @PostMapping
     public EntityModel<?> addStudent(@Valid @RequestBody Student student) {
-        Link self = linkTo(methodOn(StudentController.class).getStudent(student.getId())).withRel("self");
         Student result = studentService.addStudent(student);
-        return EntityModel.of(result, self);
+        return EntityModel.of(
+                result,
+                Link.of("http://localhost:8080/students/" + student.getId()).withRel("self"),
+                Link.of("http://localhost:8080/students").withRel("all")
+
+        );
     }
 }
